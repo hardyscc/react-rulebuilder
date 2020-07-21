93446beb1c89c39d0b27b698c59bdd02562af437
Hardys <hardyscc@gmail.com>
2020-07-21 14:47:00 +0800

    chore: add .prettierrc for vscode

--- src/components/__tests__/RuleBuilder.spec.tsx
+++ src/components/__tests__/RuleBuilder.spec.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'
-import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+import * as React from 'react'
 import { RuleBuilder } from '../../index'
 
 test('Renders', async () => {
